da8eb3f03713b77997d0ba78c9d5e4b245fc60f5
Filipe Oliveira <hi@fmoliveira.dev>
2021-05-08 23:25:46 -0300

    Create and deploy new token

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { ethers } from "ethers";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
 
 // localhost
-// const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 // ropsten
-const greeterAddress = "0xdC743fb62977B1b3b69459CA9797CDB01e733c31";
+// const greeterAddress = "0xdC743fb62977B1b3b69459CA9797CDB01e733c31";
 
 declare global {
   var ethereum: ethers.providers.ExternalProvider;
