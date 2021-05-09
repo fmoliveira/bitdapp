9c71147b4f39c56de60a81534d5e9d379ea07230
Filipe Oliveira <hi@fmoliveira.dev>
2021-05-09 17:12:12 -0300

    Create and deploy standard ERC20 token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
+  // default greeter project (aka hello world)
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
   await greeter.deployed();
   console.log("Greeter deployed to:", greeter.address);
 
-  const Token = await hre.ethers.getContractFactory("Token");
-  const token = await Token.deploy();
+  // custom token
+  // const Token = await hre.ethers.getContractFactory("Token");
+  // const token = await Token.deploy();
+  // await token.deployed();
+  // console.log("Token deployed to:", token.address);
+
+  // ERC20 token
+  const Token = await hre.ethers.getContractFactory("FMOToken");
+  const token = await Token.deploy(100000); // initial supply
   await token.deployed();
   console.log("Token deployed to:", token.address);
 }
